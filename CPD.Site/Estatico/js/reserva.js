dcbf0b4599e642530651dfd7bd33d8c7c97a5be5
JpedroSnts <jp.siq.snts@gmail.com>
2023-09-26 19:36:36 -0300

    Remoção de reservas ao alterar data

--- CPD.Site/Estatico/js/reserva.js
+++ CPD.Site/Estatico/js/reserva.js
@@ -1,37 +1,55 @@
 window.addEventListener("load", () => {
-	const btnAmbiente = document.querySelector("#Ambiente");
-	const btnEquipamento = document.querySelector("#Equipamento");
-	const pnlAmbientes = document.querySelector("#pnlAmbientes");
-	const pnlEquipamentos = document.querySelector("#pnlEquipamentos");
+    const btnAmbiente = document.querySelector("#Ambiente");
+    const btnEquipamento = document.querySelector("#Equipamento");
+    const pnlAmbientes = document.querySelector("#pnlAmbientes");
+    const pnlEquipamentos = document.querySelector("#pnlEquipamentos");
 
-	pnlEquipamentos.childNodes.forEach(e => {
-		if (e.tagName == "DIV") {
-			const qts = e.querySelector("input.numQtEquip");
-			qts.setAttribute("readonly", "readonly");
-			e.querySelector(".iconMais").addEventListener("click", e => {
-				qts.setAttribute("value", Number(qts.value) == Number(qts.getAttribute("maxlength")) ? Number(qts.getAttribute("maxlength")) : Number(qts.value) + 1);
-			});
-			e.querySelector(".iconMenos").addEventListener("click", e => {
-				qts.setAttribute("value", Number(qts.value) == 0 ? 0 : Number(qts.value) - 1);
-			});
-		}
-	});
+    pnlEquipamentos.childNodes.forEach(e => {
+        if (e.tagName == "DIV") {
+            const qts = e.querySelector("input.numQtEquip");
+            qts.setAttribute("readonly", "readonly");
+            e.querySelector(".iconMais").addEventListener("click", e => {
+                qts.setAttribute("value", Number(qts.value) == Number(qts.getAttribute("maxlength")) ? Number(qts.getAttribute("maxlength")) : Number(qts.value) + 1);
+            });
+            e.querySelector(".iconMenos").addEventListener("click", e => {
+                qts.setAttribute("value", Number(qts.value) == 0 ? 0 : Number(qts.value) - 1);
+            });
+        }
+    });
 
-	if (document.documentElement.clientWidth < 800) {
-		btnEquipamento.style.color = "#114a55";
+    if (document.documentElement.clientWidth < 800) {
+        btnEquipamento.style.color = "#114a55";
 
-		btnAmbiente.addEventListener("click", e => {
-			pnlAmbientes.style.display = "flex";
-			pnlEquipamentos.style.display = "none";
-			btnEquipamento.style.color = "#000";
-			btnAmbiente.style.color = "#114a55";
-		});
+        btnAmbiente.addEventListener("click", e => {
+            pnlAmbientes.style.display = "flex";
+            pnlEquipamentos.style.display = "none";
+            btnEquipamento.style.color = "#000";
+            btnAmbiente.style.color = "#114a55";
+        });
 
-		btnEquipamento.addEventListener("click", e => {
-			pnlAmbientes.style.display = "none";
-			pnlEquipamentos.style.display = "flex";
-			btnEquipamento.style.color = "#114a55";
-			btnAmbiente.style.color = "#000";
-		});
-	}
+        btnEquipamento.addEventListener("click", e => {
+            pnlAmbientes.style.display = "none";
+            pnlEquipamentos.style.display = "flex";
+            btnEquipamento.style.color = "#114a55";
+            btnAmbiente.style.color = "#000";
+        });
+    }
+
+    const txtInputData = document.querySelector("#txtInputData");
+    const txtHorarioInicio = document.querySelector("#txtHorarioInicio");
+    const txtHorarioFim = document.querySelector("#txtHorarioFim");
+    const btnReservar = document.querySelector("#btnReservar");
+
+    function limparItensEBloquearBotao() {
+        btnReservar.setAttribute("disabled", "disabled");
+        pnlAmbientes.innerHTML = "";
+        pnlEquipamentos.innerHTML = "";
+    }
+
+    txtInputData.addEventListener("keyup", limparItensEBloquearBotao);
+    txtHorarioInicio.addEventListener("keyup", limparItensEBloquearBotao);
+    txtHorarioFim.addEventListener("keyup", limparItensEBloquearBotao);
+    txtInputData.addEventListener("change", limparItensEBloquearBotao);
+    txtHorarioInicio.addEventListener("change", limparItensEBloquearBotao);
+    txtHorarioFim.addEventListener("change", limparItensEBloquearBotao);
 });
