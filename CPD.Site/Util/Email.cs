4c5000b470df8ad0e3215f26a994eac07ae45e0e
JpedroSnts <jp.siq.snts@gmail.com>
2023-10-06 19:06:00 -0300

    Função para envio de email

--- CPD.Site/Util/Email.cs
+++ CPD.Site/Util/Email.cs
@@ -1,11 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Web;
 
 namespace CPD.Site.Util
 {
-    public class Email
+    public static class Email
     {
+        public static bool Enviar(string destinatario, string assunto, string texto)
+        {
+            string nomeRemetente = "Contato CPD";
+            string remetente = "cpd_contato_teste@outlook.com";
+            string senha = "SENHA DO EMAIL";
+
+            SmtpClient client = new SmtpClient
+            {
+                Credentials = new NetworkCredential(remetente, senha),
+                Host = "smtp-mail.outlook.com",
+                Port = 587,
+                EnableSsl = true
+            };
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add(destinatario);
+            mail.From = new MailAddress(remetente, nomeRemetente, System.Text.Encoding.UTF8);
+            mail.Subject = assunto;
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = $"<html><body>{texto}</body></html>";
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+
+            try
+            {
+                client.Send(mail);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
