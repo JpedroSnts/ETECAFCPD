bf1a8d9399fc5c75a08adcc09a663f89cde42445
JpedroSnts <jp.siq.snts@gmail.com>
2023-10-10 10:09:32 -0300

    Criação de classes para gerencimento de token de alteração de senha

---
+++ CPD.Repositorio/Controller/TokenController.cs
@@ -0,0 +1,62 @@
+﻿using CPD.Repositorio.Banco;
+using CPD.Repositorio.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPD.Repositorio.Controller
+{
+    public class TokenController : ConexaoBanco
+    {
+        public Token GerarToken(string Email)
+        {
+            string token = Guid.NewGuid().ToString().Replace("-", "");
+            List<Parametro> parametros = new List<Parametro>
+            {
+                new Parametro("pEmailUsuario", Email),
+                new Parametro("pCodigoToken", token)
+            };
+            Executar("gerarToken", parametros);
+            Desconectar();
+
+            return new Token() { Email = Email, Codigo = token };
+        }
+
+        public Token BuscarToken(string token)
+        {
+            List<Parametro> parametros = new List<Parametro>
+            {
+                new Parametro("pCodigoToken", token)
+            };
+            MySqlDataReader reader = Executar("buscarToken", parametros);
+
+            Token tkn = new Token();
+            if (reader.Read())
+            {
+                tkn.Email = reader.GetString("nm_email");
+                tkn.Codigo = reader.GetString("cd_token");
+                tkn.Data = DateTime.Parse(reader.GetString("dt_token"));
+            }
+
+            if (!reader.IsClosed) reader.Close();
+            Desconectar();
+
+            return tkn;
+        }
+
+        public void AlterarSenha(string token, string novaSenha, string confirmacaoSenha)
+        {
+            List<Parametro> parametros = new List<Parametro>
+            {
+                new Parametro("pCodigoToken", token),
+                new Parametro("pNovaSenha", novaSenha),
+                new Parametro("pConfirmacaoSenha", confirmacaoSenha)
+            };
+            Executar("alterarSenhaToken", parametros);
+            Desconectar();
+        }
+    }
+}
---
+++ CPD.Repositorio/Model/Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPD.Repositorio.Model
+{
+    public class Token
+    {
+        public string Email { get; set; }
+        public string Codigo { get; set; }
+        public DateTime Data {  get; set; }
+    }
+}
