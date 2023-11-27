497f15acf9f63345985ac3e216b714e2d19fcaa4
JpedroSnts <jp.siq.snts@gmail.com>
2023-11-27 10:02:22 -0300

    Correção exclusão uso ambiente

--- CPD.Site/Estatico/js/gradeHorario.js
+++ CPD.Site/Estatico/js/gradeHorario.js
@@ -1,11 +1,11 @@
 ﻿window.addEventListener("load", () => {
-    const btns = document.querySelectorAll(".btnExcluirUsoAmbiente");
+    const btns = document.querySelectorAll("#iconLixeira");
     btns.forEach((el) => {
         el.addEventListener("click", (e) => {
             e.preventDefault();
-            const dia = e.target.getAttribute("dia");
-            const inicio = e.target.getAttribute("inicio");
-            const fim = e.target.getAttribute("fim");
+            const dia = e.target.parentNode.getAttribute("dia");
+            const inicio = e.target.parentNode.getAttribute("inicio");
+            const fim = e.target.parentNode.getAttribute("fim");
             fetch(`/Api/excluirGradeHorario.aspx?dia=${dia}&inicio=${inicio}&fim=${fim}`).then(() => {
                 location.reload();
             });
